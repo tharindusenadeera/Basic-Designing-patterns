898c3808b67fd69fb7e5990f1b2972e6fe40380a
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-07-24 23:28:15 +0100

    Added required classes

---
+++ src/filterMethod/OrCriteria.java
@@ -0,0 +1,13 @@
+package filterMethod;
+
+import java.util.List;
+
+public class OrCriteria implements Criteria{
+
+    private Criteria criteria;
+    private Criteria otherCriteria;
+    @Override
+    public List<Person> meetCriteria(List<Person> persons) {
+        return null;
+    }
+}
