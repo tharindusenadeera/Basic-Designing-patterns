797a7530cde01957b01867b60e6819ee5460cfb4
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-07-26 16:39:41 +0100

    Composite method implemented with an example

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import bridgeMethod.GreenCircle;
 import bridgeMethod.RedCircle;
 import builderMethod.Phone;
 import builderMethod.PhoneBuilder;
+import compositeMethod.Employee;
 import factoryMethod.Client;
 import factoryMethod.Vehicle;
 import factoryMethod.VehicleFactory;
@@ -96,6 +97,38 @@ public class Main {
 
         System.out.println("\nSingle Or Females: ");
         printPersons(singleOrFemale.meetCriteria(persons));
+
+        // With Composite method example
+        Employee CEO = new Employee("John","CEO", 30000);
+        Employee headSales = new Employee("Robert","Head Sales", 20000);
+
+        Employee headMarketing = new Employee("Michel","Head Marketing", 20000);
+
+        Employee clerk1 = new Employee("Laura","Marketing", 10000);
+        Employee clerk2 = new Employee("Bob","Marketing", 10000);
+
+        Employee salesExecutive1 = new Employee("Richard","Sales", 10000);
+        Employee salesExecutive2 = new Employee("Rob","Sales", 10000);
+
+        CEO.add(headSales);
+        CEO.add(headMarketing);
+
+        headSales.add(salesExecutive1);
+        headSales.add(salesExecutive2);
+
+        headMarketing.add(clerk1);
+        headMarketing.add(clerk2);
+
+        //print all employees of the organization
+        System.out.println(CEO);
+
+        for (Employee headEmployee : CEO.getSubordinates()) {
+            System.out.println(headEmployee);
+
+            for (Employee employee : headEmployee.getSubordinates()) {
+                System.out.println(employee);
+            }
+        }
     }
 
     public static void printPersons(List<Person> persons){
