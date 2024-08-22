e46e56e1cef06d626b1656a5fb81c3496a857b4c
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-08-22 22:49:05 +0100

    Transfer object method implemented with an example

--- src/Main.java
+++ src/Main.java
@@ -50,6 +50,8 @@ import strategyMethod.OperationSubtract;
 import templateClass.Cricket;
 import templateClass.Football;
 import templateClass.Game;
+import transferObjectMethod.StudentBO;
+import transferObjectMethod.StudentVO;
 import visitorMethod.Computer;
 import visitorMethod.ComputerPartDisplayVisitor;
 import visitorMethod.IComputerPart;
@@ -325,6 +327,22 @@ public class Main {
 
         robert.sendMessage("Hi! John!");
         john.sendMessage("Hello! Robert!");
+
+        // With Transfer Object method example
+        StudentBO studentBusinessObject = new StudentBO();
+        //print all students
+        for (StudentVO student : studentBusinessObject.getAllStudents()) {
+            System.out.println("Student: [RollNo : " + student.getRollNo() + ", Name : " + student.getName() + " ]");
+        }
+
+        //update student
+        StudentVO student = studentBusinessObject.getAllStudents().get(0);
+        student.setName("Michael");
+        studentBusinessObject.updateStudent(student);
+
+        //get the student
+        student = studentBusinessObject.getStudent(0);
+        System.out.println("Student: [RollNo : " + student.getRollNo() + ", Name : " + student.getName() + " ]");
     }
 
     public static void printPersons(List<Person> persons){
