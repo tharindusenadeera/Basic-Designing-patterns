58b6b7c1a1451e2cd49488084c6e89ac68fdf1fa
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-08-27 23:31:57 +0100

    Chain of responsibility method implemented with an example

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ import bridgeMethod.GreenCircle;
 import bridgeMethod.RedCircle;
 import builderMethod.Phone;
 import builderMethod.PhoneBuilder;
+import chainOfResponsibilityMethod.AbstractLogger;
+import chainOfResponsibilityMethod.ConsoleLogger;
+import chainOfResponsibilityMethod.ErrorLogger;
+import chainOfResponsibilityMethod.FileLogger;
 import commandMethod.Broker;
 import commandMethod.BuyStock;
 import commandMethod.SellStock;
@@ -379,6 +383,30 @@ public class Main {
         service.execute();
         service = ServiceLocator.getService("Service2");
         service.execute();
+
+        // with Chain of responsibility method example
+        AbstractLogger loggerChain = getChainOfLoggers();
+
+        loggerChain.logMessage(AbstractLogger.INFO,
+                "This is an information.");
+
+        loggerChain.logMessage(AbstractLogger.DEBUG,
+                "This is an debug level information.");
+
+        loggerChain.logMessage(AbstractLogger.ERROR,
+                "This is an error information.");
+    }
+
+    private static AbstractLogger getChainOfLoggers(){
+
+        AbstractLogger errorLogger = new ErrorLogger(AbstractLogger.ERROR);
+        AbstractLogger fileLogger = new FileLogger(AbstractLogger.DEBUG);
+        AbstractLogger consoleLogger = new ConsoleLogger(AbstractLogger.INFO);
+
+        errorLogger.setNextLogger(fileLogger);
+        fileLogger.setNextLogger(consoleLogger);
+
+        return errorLogger;
     }
 
     public static void printPersons(List<Person> persons){
