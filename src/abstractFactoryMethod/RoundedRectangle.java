6533bb01208b749a083949dd462c9bd389d5b224
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-09-06 23:58:57 +0100

    Commented

--- src/abstractFactoryMethod/RoundedRectangle.java
+++ src/abstractFactoryMethod/RoundedRectangle.java
@@ -3,6 +3,6 @@ package abstractFactoryMethod;
 public class RoundedRectangle implements Shape{
     @Override
     public void draw() {
-        System.out.println("Inside RoundedRectangle::draw() method.");
+//        System.out.println("Inside RoundedRectangle::draw() method.");
     }
 }
